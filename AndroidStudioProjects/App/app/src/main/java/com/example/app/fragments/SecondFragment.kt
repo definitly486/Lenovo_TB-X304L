549dd21accaf18985906229f6bbd0d619cf9f642
Your Name <you@example.com>
2025-10-09 02:34:04 +0300

    изменено:      AndroidStudioProjects/App/app/src/main/java/com/example/app/fragments/SecondFragment.kt

--- AndroidStudioProjects/App/app/src/main/java/com/example/app/fragments/SecondFragment.kt
+++ AndroidStudioProjects/App/app/src/main/java/com/example/app/fragments/SecondFragment.kt
@@ -11,11 +11,34 @@ import com.example.app.R
 
 class SecondFragment : Fragment() {
 
+    var apkHttpUrl = "https://github.com/definitly486/Lenovo_TB-X304L/releases/download/apk/"
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_second, container, false)
         val button = view.findViewById<Button>(R.id.button5)
         button.setOnClickListener {
-            // Реакция на нажатие второй кнопки
+            val helper = DownloadHelper(requireContext())
+            helper.download("${apkHttpUrl}app_mpv-default-arm64-v8a-release.apk")
+            helper.download("${apkHttpUrl}ByeByeDPI-arm64-v8a-release.apk")
+            helper.download("${apkHttpUrl}File+Manager++-Premium-v3.5.4_2103054-Mod.apk")
+            helper.download("${apkHttpUrl}vcore.apk")
+            helper.download("${apkHttpUrl}com.aurora.store_70.apk")
+            helper.download("${apkHttpUrl}NewPipe_nightly-1066.apk")
+            helper.download("${apkHttpUrl}org-gnucash-android-24003-39426726-deeea690953a751a05a1a35017540c33.apk")
+            helper.download("${apkHttpUrl}com.android.chrome_141.0.7390.43-739004331_1lang_2feat_e182f1b6bd7ad7d24534108e4e98f9ed_apkmirror.com.apkm")
+            helper.download("${apkHttpUrl}sports+2024_1.2_apkcombo.com_antisplit.apk")
+            helper.download("${apkHttpUrl}Hacker_v1.41.1.apk")
+            helper.download("${apkHttpUrl}terminal.apk")
+        }
+        val button6 = view.findViewById<Button>(R.id.button6)
+        button6.setOnClickListener {
+            val helper = DownloadHelper(requireContext())
+            helper.installApk("vcore.apk")
+            helper.installApk("NewPipe_nightly-1066.apk")
+            helper.installApk("File+Manager++-Premium-v3.5.4_2103054-Mod.apk")
+            helper.installApk("app_mpv-default-arm64-v8a-release.apk")
+            helper.installApk("ByeByeDPI-arm64-v8a-release.apk")
         }
         return view
     }
