1e3c5ecf66c05860b49b86c743f98473f7812e81
Your Name <you@example.com>
2025-10-09 10:06:45 +0300

    новый файл:    release_apk.sh

---
+++ release_apk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+APK_PATH="$HOME/AndroidStudioProjects/App/app/build/outputs/apk/debug"
+cd $HOME/Lenovo_TB-X304L
+mv $APK_PATH/app-debug.apk  $APK_PATH/app304-android10.apk
+gh release  delete-asset apk  app304-android10.apk 
+gh release  upload apk $APK_PATH/app304-android10.apk
