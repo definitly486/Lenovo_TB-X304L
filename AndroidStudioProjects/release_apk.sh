7024f7ea171800d07f3d7382aada6e21bde7c785
Your Name <you@example.com>
2025-10-14 02:35:16 +0300

    удалено:       AndroidStudioProjects/304/.gitignore 	удалено:       AndroidStudioProjects/304/app/.gitignore 	удалено:       AndroidStudioProjects/304/app/build.gradle.kts 	удалено:       AndroidStudioProjects/304/app/proguard-rules.pro 	удалено:       AndroidStudioProjects/304/app/src/androidTest/java/com/example/a304/ExampleInstrumentedTest.kt

--- AndroidStudioProjects/release_apk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-APK_PATH="$HOME/AndroidStudioProjects/App/app/build/outputs/apk/debug"
-cd $HOME/Lenovo_TB-X304L
-mv $APK_PATH/app-debug.apk  $APK_PATH/app304-android10.apk
-echo "Y" | gh release  delete-asset apk  app304-android10.apk 
-gh release  upload apk $APK_PATH/app304-android10.apk
